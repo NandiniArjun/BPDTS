366e4fb44ee55c50854a859403f275931b6eb189
NandiniArjun <30292911+NandiniArjun@users.noreply.github.com>
2020-01-22 17:18:29 +0000

    Create BPDTSApplication.java

---
+++ src/main/java/com/bpdts/rest/BPDTSApplication.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright © 2020 Lhasa Limited
+ * File created: 21/01/2020 by Nandini
+ * Creator : Nandini
+ * Version : $$Id$$
+ */
+package com.bpdts.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nandini
+ * @since 21/01/2020
+ */
+@SpringBootApplication
+public class BPDTSApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(BPDTSApplication.class, args);
+	}
+}
